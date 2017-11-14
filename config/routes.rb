75115d65ef2934768bb05dc97f0855070ebd951a
Michelle Brien <michelle.brien12@gmail.com>
2017-11-14 15:02:30 -0200

    CRUD implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/index'
+  resources:articles
 
-  get 'articles/show'
-
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/edit'
-
-  get 'articles/update'
-
-  get 'articles/destroy'
+  root to: 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
